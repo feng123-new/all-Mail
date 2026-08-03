a6722478b0aa744e5304652a2b1f1b97a9fc5575
feng123-new <3494617655@qq.com>
2026-08-04 01:48:01 +0800

    feat(web): export mail-flow context primitives

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -4,6 +4,9 @@ export { default as PageSurface } from './PageSurface';
 export { default as SurfaceCard } from './SurfaceCard';
 export { default as AuthSplitLayout } from './AuthSplitLayout';
 export { default as LanguageToggle } from './LanguageToggle';
+export { default as MailFlowContext } from './MailFlowContext';
+export { default as MailFlowStatusBadge, getMailFlowStateTone } from './MailFlowStatusBadge';
+export type { MailFlowState, MailFlowSurface } from './MailFlowContext';
 export {
   DataToolbar,
   SectionHeading,
