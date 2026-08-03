e01729cba21ca162921702272a05793dd7476ff7
feng123-new <3494617655@qq.com>
2026-08-04 01:52:31 +0800

    refactor(web): export mail-flow model from utility module

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -5,8 +5,9 @@ export { default as SurfaceCard } from './SurfaceCard';
 export { default as AuthSplitLayout } from './AuthSplitLayout';
 export { default as LanguageToggle } from './LanguageToggle';
 export { default as MailFlowContext } from './MailFlowContext';
-export { default as MailFlowStatusBadge, getMailFlowStateTone } from './MailFlowStatusBadge';
-export type { MailFlowState, MailFlowSurface } from './MailFlowContext';
+export { default as MailFlowStatusBadge } from './MailFlowStatusBadge';
+export { getMailFlowStateTone } from './mailFlow';
+export type { MailFlowState, MailFlowSurface } from './mailFlow';
 export {
   DataToolbar,
   SectionHeading,
